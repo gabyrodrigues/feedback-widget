3a3e06d880450302bd7640b0bfd6958dbd16a438
gabyrodrigues <gabyrlima.99@gmail.com>
2022-05-04 23:20:44 -0300

    sending emails with created feedback

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,20 @@
 import express from 'express'
 import { prisma } from './prisma'
 
+import nodemailer from 'nodemailer'
+
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "5fa4f78f95c79c",
+    pass: "d557ef73e1567f"
+  }
+})
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
@@ -14,6 +25,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot
     }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedback Widget <oi@feedback.com>',
+    to: 'Gabriely Rodrigues <gabrielyrodrigues@alu.ufc.br>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
   })
 
   return res.status(201).json({ data: feedback })
